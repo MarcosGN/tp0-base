fd69861b478e5927d1a80b076549a04e8a44facc
MarcosGN <mgarcian@fi.uba.ar>
2026-03-14 19:34:55 -0300

    Actualizo los cambios en el script generador de docker-compose

--- generar-compose.py
+++ generar-compose.py
@@ -16,7 +16,6 @@ services:
     entrypoint: python3 /main.py
     environment:
       - PYTHONUNBUFFERED=1
-      - LOGGING_LEVEL=DEBUG
     volumes:
       - ./server/config.ini:/config.ini
     networks:
@@ -30,7 +29,6 @@ for i in range(1, client_count + 1):
     entrypoint: /client
     environment:
       - CLI_ID={i}
-      - CLI_LOG_LEVEL=DEBUG
     volumes:
       - ./client/config.yaml:/config.yaml
     networks:
