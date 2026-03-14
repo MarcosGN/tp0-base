1e07e49e06ec8e9dbb4f9eac7810a8d557511f6e
MarcosGN <mgarcian@fi.uba.ar>
2026-03-14 19:14:30 -0300

    Creo volumenes que mapean los archivos de configuracion del docker host al container

--- generar-compose.py
+++ generar-compose.py
@@ -17,6 +17,8 @@ services:
     environment:
       - PYTHONUNBUFFERED=1
       - LOGGING_LEVEL=DEBUG
+    volumes:
+      - ./server/config.ini:/config.ini
     networks:
       - testing_net
 """
@@ -29,6 +31,8 @@ for i in range(1, client_count + 1):
     environment:
       - CLI_ID={i}
       - CLI_LOG_LEVEL=DEBUG
+    volumes:
+      - ./client/config.yaml:/config.yaml
     networks:
       - testing_net
     depends_on:
