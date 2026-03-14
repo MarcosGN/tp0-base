b188622e94f2870e2de505ec544ee5a24e496ce8
MarcosGN <mgarcian@fi.uba.ar>
2026-03-14 13:08:09 -0300

    Creando el script que genera el archivo docker-compose con cantidad de clientes personalizable

---
+++ generar-compose.py
@@ -0,0 +1,49 @@
+import sys
+
+if len(sys.argv) != 3:
+    print("Usage: python3 generar-compose.py <output_file> <client_count>")
+    sys.exit(1)
+
+output_file = sys.argv[1]
+client_count = int(sys.argv[2])
+
+# Generate the compose file content
+compose_content = f"""name: tp0
+services:
+  server:
+    container_name: server
+    image: server:latest
+    entrypoint: python3 /main.py
+    environment:
+      - PYTHONUNBUFFERED=1
+      - LOGGING_LEVEL=DEBUG
+    networks:
+      - testing_net
+"""
+for i in range(1, client_count + 1):
+    compose_content += f"""
+  client{i}:
+    container_name: client{i}
+    image: client:latest
+    entrypoint: /client
+    environment:
+      - CLI_ID={i}
+      - CLI_LOG_LEVEL=DEBUG
+    networks:
+      - testing_net
+    depends_on:
+      - server
+"""
+    
+compose_content += """
+networks:
+  testing_net:
+    ipam:
+      driver: default
+      config:
+        - subnet: 172.25.125.0/24
+"""
+
+# Write the compose file
+with open(output_file, "w") as f:
+    f.write(compose_content)
